5ead01f3eab1b2b5072f31862549dedb785c2f68
eswise <eswise@gmail.com>
2023-09-16 15:25:33 -0400

    Grocery List Solution

---
+++ 301/Exercises/GroceryList/Solution/ListManager.cs
@@ -0,0 +1,34 @@
+﻿namespace GroceryList
+{
+    public class ListManager
+    {
+        private List<GroceryItem> _groceryList;
+
+        public ListManager()
+        {
+            _groceryList = new List<GroceryItem>();
+        }
+
+        public void AddItem(GroceryItem item)
+        {
+            _groceryList.Add(item);
+        }
+
+        public GroceryItem RemoveItem(int number)
+        {
+            int index = number - 1;
+
+            var item = _groceryList[index];
+            _groceryList.RemoveAt(index);
+
+            return item;
+        }
+
+        public int ItemCount { get { return _groceryList.Count; } }
+
+        public void DisplayItems()
+        {
+            ConsoleIO.DisplayItems(_groceryList);
+        }
+    }
+}
