7ad4324bd29465c90163e7060ba7047d3126497d
eswise <eswise@gmail.com>
2023-10-01 12:32:33 -0400

    Finished method syntax

--- 301/Examples/LINQExamples/MethodSyntax.cs
+++ 301/Examples/LINQExamples/MethodSyntax.cs
@@ -10,6 +10,13 @@
             ConsoleIO.PrintEmployees(activeEmployees, "Active Employees");
         }
 
+        public static void WhereExample2()
+        {
+            var everyOtherEmployee = _employees.Where((e, index) => index % 2 == 0);
+            
+            ConsoleIO.PrintEmployees(everyOtherEmployee, "Every Other Employee");
+        }
+
         public static void All()
         {
             Console.WriteLine("Are all employees managers?");
@@ -22,10 +29,46 @@
             Console.WriteLine(_employees.Any(e => e.Title == "Manager"));
         }
 
+        public static void Contains()
+        {
+            Console.WriteLine("Does employee with badge id 105 exist?");
+            Console.WriteLine(_employees.Contains(new Employee { BadgeID = 105 }));
+        }
+
+        public static void Aggregates1()
+        {
+            var minSalary = _employees.Min(e => e.Salary);
+            var maxSalary = _employees.Max(e => e.Salary);
+            var avgSalary = _employees.Average(e => e.Salary);
+            var sumSalary = _employees.Sum(e => e.Salary);
+            var countSalary = _employees.Count(e => e.Salary.HasValue);
+
+            Console.WriteLine($"{minSalary}, {maxSalary}, {avgSalary}, {sumSalary}, {countSalary}");
+        }
+
+        public static void Aggregates2()
+        {
+            int[] nums = { 1, 3, 5, 7, 9 };
+            var min = nums.Min();
+            var max = nums.Max();
+            var avg = nums.Average();
+            var sum = nums.Sum();
+            var count = nums.Count();
+
+            Console.WriteLine($"{min}, {max}, {avg}, {sum}, {count}");
+        }
+
         public static void First()
         {
             var firstEmployeeInCollection = _employees.First(e => e.Department == "IT");
             ConsoleIO.PrintEmployee(firstEmployeeInCollection, "First Employee in IT");
+        }
+
+        public static void First2()
+        {
+            // this will throw an exception
+            var firstEmployeeInCollection = _employees.First(e => e.Department == "Fake");
+            ConsoleIO.PrintEmployee(firstEmployeeInCollection, "This will error");
         }
 
         public static void Last()
@@ -44,6 +87,7 @@
             var bySalaryDescending = _employees.Where(e => e.HourlyRate.HasValue)
                 .OrderBy(e => e.HourlyRate)
                 .ThenBy(e => e.LastName);
+
             ConsoleIO.PrintEmployees(bySalaryDescending, "Hourly Employees By Lowest Rate");
         }
 
@@ -52,6 +96,7 @@
             var bySalaryDescending = _employees.Where(e => e.Salary.HasValue)
                 .OrderByDescending(e => e.Salary)
                 .ThenBy(e => e.LastName);
+
             ConsoleIO.PrintEmployees(bySalaryDescending, "Salaried Employees By Highest Salary");
         }
 
@@ -63,17 +108,41 @@
 
         public static void Grouping()
         {
-            var deptEmployees = _employees.GroupBy(e => e.Department);
+            var groupedEmployees = _employees.GroupBy(e => e.Department);
 
-            foreach(var dept in deptEmployees)
+            foreach(var group in groupedEmployees)
             {
-                Console.WriteLine(new string('=', 25));
-                Console.WriteLine($"Department: {dept.Key}");
-                foreach(var employee in dept)
+                Console.WriteLine($"Department: {group.Key}");
+                foreach(var employee in group)
                 {
-                    Console.WriteLine($"{employee.LastName}, {employee.FirstName}");
+                    Console.WriteLine(
+                        $"\t{employee.LastName}, {employee.FirstName}");
                 }
-                Console.WriteLine(new string('=', 25));
+            }
+        }
+
+        public static void SelectCSV()
+        {
+            var csv = _employees.Select(e => 
+                $"{e.BadgeID},{e.LastName},{e.FirstName},{e.Department}");
+
+            foreach(var line in csv)
+            {
+                Console.WriteLine(line);
+            }           
+        }
+
+        public static void SelectAnonymous()
+        {
+            var dropDownList = _employees.Select(e => new
+            {
+                Text = $"{e.LastName}, {e.FirstName}",
+                Value = e.BadgeID
+            });
+
+            foreach (var item in dropDownList)
+            {
+                Console.WriteLine($"{item.Value} - {item.Text}");
             }
         }
     }
