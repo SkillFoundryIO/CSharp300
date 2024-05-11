2c60ec1fa5c2f46b53ad56d8c1b51e3aa397c1ca
SkillFoundryIO <eswise@gmail.com>
2024-05-11 11:21:41 -0400

    Fixed borower issue, removed instructions md file because it's in the course now.

--- 302/Exercises/LibraryManager/start/LibraryManagement/LibraryManagement.Core/Entities/Borrower.cs
+++ 302/Exercises/LibraryManager/start/LibraryManagement/LibraryManagement.Core/Entities/Borrower.cs
@@ -7,7 +7,6 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public DateTime DoB { get; set; }
     }
 
 }
